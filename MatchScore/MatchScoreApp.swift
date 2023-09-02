ebce4f814b9a8a02cbdf4b1488a061b0c689d7f6
Leandro Rodrigues <leandropcsx@gmail.com>
2023-09-02 10:43:08 -0300

    Initial Commit

---
+++ MatchScore/MatchScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchScoreApp.swift
+//  MatchScore
+//
+//  Created by Leandro Rodrigues on 02/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MatchScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
